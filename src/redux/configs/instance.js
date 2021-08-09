ccb1088edde1ef8d9e125c969a181b60fce087bc
AdamsEugene <adamseugene292@gmail.com>
2021-08-09 12:07:54 +0000

    all

--- src/redux/configs/instance.js
+++ src/redux/configs/instance.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export const instance = (accessToken) => {
   return axios.create({
-    baseURL: "https://chat-apa.herokuapp.com/",
+    baseURL: "https://chat-apa.herokuapp.com",
     // timeout: 1000,
     withCredentials: false,
     headers: {
-      "Access-Control-Allow-Origin": "https://chat-apa.herokuapp.com/",
+      "Access-Control-Allow-Origin": "https://chat-apa.herokuapp.com",
       "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
       token: accessToken ? "bearer " + accessToken : "bearer token",
     },
